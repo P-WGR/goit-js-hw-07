09843e8823c1808a9cb397e19c9241b9392203a6
P-WGR <wgr.animations@gmail.com>
2024-06-08 23:29:47 +0200

    Próbna galeria obrazów nr.1

---
+++ js/task-1.js
@@ -0,0 +1,9 @@
+const categoriesList = document.getElementById('categories');
+const items = categoriesList.querySelectorAll('.item');
+console.log(`Number of categories: ${items.length}`);
+items.forEach(item => {
+  const categoryTitle = item.querySelector('h2').textContent;
+  const categoryElements = item.querySelectorAll('ul li');
+  console.log(`Category: ${categoryTitle}`);
+  console.log(`Elements: ${categoryElements.length}`);
+});
